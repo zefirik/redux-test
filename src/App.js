35c47423a1c0e17e043df2660c8d280667940954
Artem Mialo <timredrick1@gmail.com>
2021-10-05 18:23:23 +0300

    redux-toolkit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addTodo } from './store/todoSlice';
+import TodoList from './components/TodoList';
+import InputField from './components/InputField';
+
+import './App.css';
+
+
+function App() {
+  const [text, setText] = useState('');
+  const dispatch = useDispatch();
+  const addTask = () => {
+    dispatch(addTodo({text}));
+    setText('');
+  };
+
+  return (
+    <div className="App">
+      <InputField text={text}  handleInput={setText} handleSubmit={addTask}/>
+
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
